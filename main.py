6e9e8bc45cf951aa58b64d26308e76c8c080075b
Andrea <andry.marini@gmail.com>
2021-05-08 11:07:11 +0200

    added other translation providers

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from builtins import print
 import pyperclip
 from translate import Translator
+import translators as ts
 
 if __name__ == '__main__':
 
     copied = pyperclip.paste()
     print(f'Text to be translated: {copied}')
 
-    translator = Translator(to_lang="it")
-    translation = translator.translate(copied)
+    try:
+        translation = ts.google(copied, to_language='it')
+    except:
+        pass
+    try:
+        translation = ts.bing(copied, to_language='it')
+    except:
+        try:
+            translator = Translator(to_lang="it")
+            translation = translator.translate(copied)
+        except:
+            pass
+
     print(f'Translation: {translation}')
 
     print('\n')
